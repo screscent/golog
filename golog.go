848c3c2d429b6c773a80a3956962f6c58a6e4004
screscent <gonghaohua1985@163.com>
2013-08-22 14:43:05 +0800

    Update golog.go

--- golog.go
+++ golog.go
@@ -1,9 +1,8 @@
-//form https://github.com/astaxie/beego/blob/master/log.go
-//and modify it
 package golog
 
 import (
-  "io"
+	"fmt"
+	"io"
 	"os"
 )
 
@@ -36,16 +35,16 @@ func SetLevel(l int) {
 	level = l
 }
 
+// SetFlags sets the output flags for the logger.
+func SetFlags(flag int) {
+	GoLogger.SetFlags(flag)
+}
+
 // logger references the used application logger.
 var GoLogger = New(os.Stdout, "", Ldate|Ltime|Lshortfile, 1)
 
 func SetOutput(w io.Writer) {
 	GoLogger = New(w, "", Ldate|Ltime|Lshortfile, 1)
-}
-
-// SetFlags sets the flags for the gologger.
-func SetFlags(flag int) {
-	GoLogger.SetFlags(flag)
 }
 
 // SetLogger sets a new logger.
@@ -56,41 +55,42 @@ func SetLogger(l *Logger) {
 // Trace logs a message at trace level.
 func Trace(v ...interface{}) {
 	if level <= LevelTrace {
-		GoLogger.Printf("[T] %v\n", v)
+		GoLogger.Printf("[T] %s", fmt.Sprintln(v...))
 	}
 }
 
 // Debug logs a message at debug level.
 func Debug(v ...interface{}) {
 	if level <= LevelDebug {
-		GoLogger.Printf("[D] %v\n", v)
+		GoLogger.Printf("[D] %s", fmt.Sprintln(v...))
 	}
 }
 
 // Info logs a message at info level.
 func Info(v ...interface{}) {
 	if level <= LevelInfo {
-		GoLogger.Printf("[I] %v\n", v)
+		GoLogger.Printf("[I] %s", fmt.Sprintln(v...))
 	}
 }
 
 // Warning logs a message at warning level.
 func Warn(v ...interface{}) {
 	if level <= LevelWarning {
-		GoLogger.Printf("[W] %v\n", v)
+		GoLogger.Printf("[W] %s", fmt.Sprintln(v...))
 	}
 }
 
 // Error logs a message at error level.
 func Error(v ...interface{}) {
 	if level <= LevelError {
-		GoLogger.Printf("[E] %v\n", v)
+		GoLogger.Printf("[E] %s", fmt.Sprintln(v...))
 	}
 }
 
 // Critical logs a message at critical level.
 func Critical(v ...interface{}) {
 	if level <= LevelCritical {
-		GoLogger.Printf("[C] %v\n", v)
+		GoLogger.Printf("[C] %s", fmt.Sprintln(v...))
+		os.Exit(1)
 	}
 }
